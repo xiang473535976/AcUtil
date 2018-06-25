d1f0d48ddcaac59ce307dec9d7df0b2003fa6bbe
艾晨 <473535976@qq.com>
2018-06-25 14:33:30 +0800

    1

--- util/src/main/java/x/aichen/extend/activity.kt
+++ util/src/main/java/x/aichen/extend/activity.kt
@@ -1,6 +1,9 @@
 package x.aichen.extend
 
 import android.app.Activity
+import android.app.Application
+import android.content.ComponentCallbacks
+import android.content.res.Configuration
 import android.view.View
 
 /**
@@ -25,4 +28,48 @@ fun Activity.BackClick(view: View) {
 fun <V : View> Activity.fd(_id: Int): V {
     return findViewById<V>(_id) //as V
 }
+
+
+/**
+ *   https://mp.weixin.qq.com/s/d9QCoBP6kV9VSWvVldVVwA
+ * 今日头条的屏幕适配方案
+ * 如果设计图给的是dp  建议用这种    px的话 就用autolayout
+ * designWidth 设计图宽度  下面假设设计图宽度是360dp，以宽维度来适配。
+ */
+private var sNonCompatDensity = 0f
+private var sNonCompatScaleDensity = 0f
+fun Activity.setCustomDensity(application: Application, designWidth: Int) {
+    val appDisplayMetrics = application.resources.displayMetrics;
+    if (sNonCompatDensity == 0f) {
+        sNonCompatDensity = appDisplayMetrics.density;
+        sNonCompatScaleDensity = appDisplayMetrics.scaledDensity;
+        application.registerComponentCallbacks(object : ComponentCallbacks {
+            override fun onLowMemory() {
+
+            }
+
+            override fun onConfigurationChanged(newConfig: Configuration?) {
+                sNonCompatScaleDensity = application.getResources().getDisplayMetrics().scaledDensity;
+            }
+
+        })
+
+    }
+
+//屏幕宽 的像素除以360 获得Density的值
+//    val targetDensity = appDisplayMetrics.widthPixels / 360
+    val targetDensity = appDisplayMetrics.widthPixels / designWidth
+    val targetDensityDpi = targetDensity * 160
+    val targetScaledDensity = targetDensity * (sNonCompatScaleDensity / sNonCompatDensity);
+    appDisplayMetrics.density = targetDensity.toFloat()
+    appDisplayMetrics.scaledDensity = targetScaledDensity
+    appDisplayMetrics.densityDpi = targetDensityDpi
+
+    val activityDisplayMetrics = application.resources.displayMetrics;
+    activityDisplayMetrics.density = targetDensity.toFloat()
+    activityDisplayMetrics.scaledDensity = targetScaledDensity
+    activityDisplayMetrics.densityDpi = targetDensityDpi
+
+}
+
 
