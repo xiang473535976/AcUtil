1bb19ab7e93cd329b235b9f84817b332dd3e7c88
艾晨 <473535976@qq.com>
2018-06-27 11:08:42 +0800

    1111

--- util/src/main/java/x/aichen/base/XBaseActivity.kt
+++ util/src/main/java/x/aichen/base/XBaseActivity.kt
@@ -12,6 +12,8 @@ import com.zhy.autolayout.AutoFrameLayout
 import com.zhy.autolayout.AutoLinearLayout
 import com.zhy.autolayout.AutoRelativeLayout
 import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 import x.aichen.R
 
 
@@ -145,5 +147,12 @@ abstract class XBaseActivity : RxAppCompatActivity(), BGASwipeBackHelper.Delegat
             return
         }
         mSwipeBackHelper.backward()
+    }
+
+    /**
+     * eventbus  必须一个接收器     默认随便写了个哈
+     */
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onMessageEvent(event: String) {/* Do something */
     }
 }
