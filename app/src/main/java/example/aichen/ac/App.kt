eb04ba9c0529d4f53a1fa8c3120f50fefb3de219
艾晨 <473535976@qq.com>
2018-07-13 18:12:02 +0800

    1111111111

--- app/src/main/java/example/aichen/ac/App.kt
+++ app/src/main/java/example/aichen/ac/App.kt
@@ -6,6 +6,7 @@ import x.aichen.http.RetrofitManager
 import x.aichen.http.interceptor.ParmLogInterceptor
 
 class App : XApp() {
+
     override fun onCreate() {
         super.onCreate()
         RetrofitManager.CONFIG().interceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
